2f365f418541193244cd42bfd5da18c9d32f0b85
KushagraKatiha <kushagrakatiha123@gmail.com>
2023-08-04 16:51:04 +0530

    connect to db

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
-const dbConnection = async ()=>{
-    await mongoose.connect(process.env.MONGO_URI)
+const dbConnection = ()=>{
+     mongoose.connect(process.env.MONGO_URI)
     .then((conn)=>{
         console.log(`Connected to DB: ${conn.connection.host}`);
     })
